9145e06b4e31fdace30d975dcbb3a57b915c1f85
Oluwasegun Akinpelu <82780450+oluwasege@users.noreply.github.com>
2021-10-21 09:56:38 +0100

    Done

--- Array.BinarySearch/Program.cs
+++ Array.BinarySearch/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace Array.BinarySearch
+namespace Binary
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create an array of 10 elements
+            int[] IntArray = new int[10]
+            {
+                1,3,5,7,11,13,17,19,23,31
+            };
+
+            //Value to search for
+            int target = 17;
+            int pos = Array.BinarySearch(IntArray, target);
+            if (pos >= 0)
+                Console.WriteLine($"Item {IntArray[pos].ToString()} found at position {pos + 1}.");
+            else
+                Console.WriteLine("Item not found");
+            Console.ReadKey();
         }
     }
 }
